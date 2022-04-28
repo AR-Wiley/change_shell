2a823c41f5a65f9925c5425029316232ba1f20b4
AR-Wiley <ar.wiley805@outlook.com>
2022-04-28 17:44:44 -0500

    first commit

---
+++ Shell_C.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+echo -e  "\n *** Current Shell *** \n"
+
+ps -p $$
+
+echo -e "\n *** Changing Default Shell *** \n *** To dash *** \n"
+
+
+chsh -s /bin/dash
+
+echo End Program
+
+
+
